d1923f0ba96df3fdaf814fd82f1d341c298248f1
kiran tayade <kirantayade2022@gmail.com>
2026-01-20 20:41:20 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -5,30 +5,29 @@ const port = 3000 ;
 
 app.use(express.json());
 
-// this will handle only get api to /user
-app.get("/user",(req,res)=>{
-    res.send({firstName : "Kiran", lastName: "Tayade"})
-});
-
-// this will handle only post api to /user
-app.post("/user",(req,res)=>{
-    console.log(req.body);
-    //Saving data to DB;
-
-    res.send ("Data saved Successfuly");
-    
-});
-
-//this will handle only delete api to /user
-app.delete("/user",(req, res)=>{
-    console.log("User Deleted Successfully");   
-});
-
-// this will match all API call to /test
-app.use("/test",(req,res) => {
-    res.send("Hello From test request Handler");
-});
-
+app.get(
+    "/user",
+    (req,res, next)=>{
+        console.log("Handling the route user!!");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 2!!");
+        next();
+    },
+   (req,res,next)=>{
+        console.log("Handling the route user 3!!");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 4!!");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 5!!");
+        res.send("5th Response");
+    },
+);
 
 app.listen(3000,()=>{
     console.log("server running on port :: ", port);
